b2e377cc59a4fbeba0bcedda9bb861a8dd8efb63
nafizcodes <nimtiaz@ttu.edu>
2022-08-22 22:57:27 -0500

    Adding more solution

---
+++ Subsets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        
+        subset = []
+
+        def dfs(i):
+            
+            if i >= len(nums) :
+                res.append(subset.copy())
+                
+                return
+            
+            #we are taking in the index value
+            subset.append(nums[i])
+            dfs(i+1)
+            
+            
+            #we are not taking the index value
+            subset.pop()
+            dfs(i+1)
+            
+            
+        dfs(0)
+      
+        return res
+    
