8561f317521d4b4f3924098d912123cf569d089c
nafizcodes <nimtiaz@ttu.edu>
2022-09-01 23:07:52 -0500

    Adding more solution

---
+++ Maximum_Subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]   #initialized at O index as there is also negative numbers and initializing at value 0 is wrong in that case
+        currSum = 0     
+        
+        for n in nums:       
+            if currSum < 0:
+                currSum = 0
+                
+            currSum += n
+            
+            maxSub = max(maxSub, currSum)
+            
+        return maxSub
+            
